9bc7855a1db1ae28ae2e92ba3eab570562a439da
claradominguesplaza <clara.dp@al.insper.edu.br>
2026-04-28 16:24:52 -0300

    EX. 3

--- funcoes.py
+++ funcoes.py
@@ -13,5 +13,5 @@ def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
 
 def remover_dado(dados_rolados, dados_no_estoque, dado_para_remover):
      dados_rolados.append(dados_no_estoque[dado_para_remover])
-     dados_no_estoque.pop(dados_no_estoque[dado_para_remover])
+     dados_no_estoque.pop(dado_para_remover)
      return[dados_rolados, dados_no_estoque]
