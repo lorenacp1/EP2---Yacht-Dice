b944eaeb755d6dd9eeab7d894595da61a3f9bd5c
Lorena <lorenacp1@al.insper.edu.br>
2026-04-28 15:29:06 -0300

    EX. 2

--- funcoes.py
+++ funcoes.py
@@ -5,3 +5,18 @@ def rolar_dados(dados):
         dados_r.append(random.randint(1,6))
     return dados_r
 
+def guardar_dado(dados_rolados,dados_no_estoque,dado_para_guardar):
+    lista = []
+    dados_restantes = lista[0]
+    dados_armazenados = lista[1]
+    
+    for dado_para_guardar in range(len(dados_rolados)):
+        dados_restantes = dados_rolados.pop(dado_para_guardar)
+        dados_armazenados = dados_no_estoque.append(dados_rolados[dado_para_guardar])
+        dados_restantes = lista[0]
+        dados_armazenados = lista[1]
+    return lista
+
+
+    
+
