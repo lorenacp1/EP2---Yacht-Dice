7a99c52db889a2c1bb48fbecf38af05220c6aa2b
Lorena <lorenacp1@al.insper.edu.br>
2026-04-28 15:43:01 -0300

    EX. 2

--- funcoes.py
+++ funcoes.py
@@ -7,8 +7,6 @@ def rolar_dados(dados):
 
 def guardar_dado(dados_rolados,dados_no_estoque,dado_para_guardar):
     lista = []
-    dados_restantes = lista[0]
-    dados_armazenados = lista[1]
     
     for dado_para_guardar in range(len(dados_rolados)):
         dados_restantes = dados_rolados.pop(dado_para_guardar)
