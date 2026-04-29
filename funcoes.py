dcbfd0a08448f30c9a926fd8c44414480b490d05
claradominguesplaza <clara.dp@al.insper.edu.br>
2026-04-28 21:45:06 -0300

    EX. 10

--- funcoes.py
+++ funcoes.py
@@ -78,3 +78,17 @@ def calcula_pontos_quadra(faces):
                 soma += numero
             return soma
     return 0
+
+def calcula_pontos_quina(faces):
+    contagens = []
+    for i in range(1,7):
+        quantidade = 0
+        for numero in faces:
+            if numero == i:
+                quantidade += 1
+        if quantidade >= 5:
+            soma = 0
+            for numero in faces:
+                soma += numero
+            return soma
+    return 0
